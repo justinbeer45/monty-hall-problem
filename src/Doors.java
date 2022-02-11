0676f82fcdd018ecaac04f3d08d4ea4182f22960
Justin Wheeler <justin.wheeler0914@gmail.com>
2022-02-10 19:27:27 -0700

    -doors-class Added Doors class to refactor Added winning door, shown door variables and setter/getter methods

---
+++ src/Doors.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Doors {
+    // set door variables
+    private int carDoor;
+    private int openDoor;
+
+    // getter methods
+    public int getCarDoor()
+    {
+        return this.carDoor;
+    }
+    public int getOpenDoor()
+    {
+        return this.openDoor;
+    }
+
+    // setter methods
+    public void setCarDoor(int carDoorNumber)
+    {
+        this.carDoor = carDoorNumber;
+    }
+    public void setOpenDoor(int openDoorNumber)
+    {
+        this.openDoor = openDoorNumber;
+    }
+}
